b258a1669747fd9a28ab7fdc6bd8cc8d9e17a9ec
Arctic Circle System <arcticfromclosed@gmail.com>
2020-10-08 12:22:17 -0700

    Attempt to fix Cobblestone Crafting Table bugs

--- src/main/java/com/arcticcirclesystem/craftier/CraftierMain.java
+++ src/main/java/com/arcticcirclesystem/craftier/CraftierMain.java
@@ -1,9 +1,11 @@
 package com.arcticcirclesystem.craftier;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -19,7 +21,7 @@ public class CraftierMain implements ModInitializer {
     public static final Block DARK_OAK_CRAFTING_TABLE = new CraftingTableBlock();
     public static final Block CRIMSON_CRAFTING_TABLE = new CraftingTableBlock();
     public static final Block WARPED_CRAFTING_TABLE = new CraftingTableBlock();
-    public static final Block COBBLESTONE_CRAFTING_TABLE = new CraftingTableBlock();
+    public static final Block COBBLESTONE_CRAFTING_TABLE = new Block(FabricBlockSettings.of(Material.STONE).hardness(1.5f));
 	public static final Block BAMBOO_CRAFTING_TABLE = new CraftingTableBlock();
 
     @Override
@@ -32,7 +34,8 @@ public class CraftierMain implements ModInitializer {
         registerCraftingTable(DARK_OAK_CRAFTING_TABLE, "dark_oak_crafting_table", true);
         registerCraftingTable(CRIMSON_CRAFTING_TABLE, "crimson_crafting_table", false);
         registerCraftingTable(WARPED_CRAFTING_TABLE, "warped_crafting_table", false);
-        registerCraftingTable(COBBLESTONE_CRAFTING_TABLE, "cobblestone_crafting_table", false);
+        Registry.register(Registry.BLOCK, new Identifier("craftier", "cobblestone_crafting_table"), COBBLESTONE_CRAFTING_TABLE);
+        Registry.register(Registry.ITEM, new Identifier("craftier", "cobblestone_crafting_table"), new BlockItem(COBBLESTONE_CRAFTING_TABLE, new Item.Settings().group(ItemGroup.MISC)));
 		registerCraftingTable(BAMBOO_CRAFTING_TABLE, "bamboo_crafting_table", true);
 
     }
